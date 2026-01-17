1230c72b313e2f446dcd3539827df90cac062072
jicmette <jicmettesc@gmail.com>
2026-01-16 22:47:12 -0600

    feat: implement Razor Views and data passing for HelloWorld controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,15 +5,17 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // GET: /HelloWorld/
-    public string Index()
-    {
-        return "This is my default action...";
-    }
+  // GET: /HelloWorld/
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    // GET: /HelloWorld/Welcome/
-    public string Welcome(string name, int numTimes = 1)
-    {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumbTimes is: {numTimes}");
-    }
+  // GET: /HelloWorld/Welcome/
+  public IActionResult Welcome(string name, int numTimes = 1)
+  {
+    ViewData["Message"] = "Hello " + name;
+    ViewData["NumTimes"] = numTimes;
+    return View();
+  }
 }
