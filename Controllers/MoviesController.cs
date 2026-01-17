2aa24a7f10e6976cae33a37e37cf7382fe23bfdd
jicmette <jicmettesc@gmail.com>
2026-01-17 01:19:28 -0600

    feat: add search by title and year filter to Movies index.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,9 +20,28 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string? searchString, int? searchYear)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (searchYear.HasValue && searchYear.Value >0)
+            {
+                movies = movies.Where(y => y.ReleaseDate.Year == searchYear.Value);
+            }
+
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
